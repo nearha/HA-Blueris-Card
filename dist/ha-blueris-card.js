ce5faad08f6b9b69154b04c646911b573fc68a97
nearha <114181979+nearha@users.noreply.github.com>
2026-05-19 13:12:23 -0300

    Fix Lovelace visual editor entrypoint

--- dist/ha-blueris-card.js
+++ dist/ha-blueris-card.js
@@ -1,1 +1,10 @@
 import "./blueiris-ui3-card.js";
+
+const BlueIrisUi3Card = customElements.get("blueiris-ui3-card");
+if (BlueIrisUi3Card) {
+  BlueIrisUi3Card.getConfigElement = function getConfigElement() {
+    return document.createElement("blueiris-ui3-card-editor");
+  };
+}
+
+console.info("%c HA-BLUERIS-CARD %c editor patch loaded ", "color:white;background:#1565c0;font-weight:700", "color:#1565c0;background:white;font-weight:700");
